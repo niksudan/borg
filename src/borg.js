bc13a0b80a12d8de05510409a32e882ecb8ff348
Nik Sudan <niksudan@gmail.com>
2016-04-29 02:01:46 +0100

    Able to tell puns

--- src/borg.js
+++ src/borg.js
@@ -4,6 +4,7 @@ var borg = {
 	responses: [
 		[/\bh(e(y|llo)|i)\b/i, 'social', 'greet'],
 		[/\bflip (a )?coin\b/, 'games', 'coinflip'],
+		[/\b((give|tell|send|post) (me )?)?(a )?(([A-Za-z ]*) )?pun( about ([A-Za-z ]*))?\b/, 'social', 'pun'],
 	],
 	
 	handle: function(message, response)
@@ -13,7 +14,7 @@ var borg = {
 			if (!responded && message.match(this.responses[i][0])) {
 				if (borg.mention && message.indexOf(borg.mention) > -1) {
 					responded = true;
-					this[this.responses[i][1]][this.responses[i][2]](response);
+					this[this.responses[i][1]][this.responses[i][2]](response, message, this.responses[i][0]);
 					break;
 				}
 			}
--- src/social.js
+++ src/social.js
@@ -1,8 +1,38 @@
+var http = require('http');
+
 module.exports = {
 	
 	// Say hi
 	greet: function(response)
 	{
 		response('Hello!');
+	},
+	
+	// Tell a pun
+	pun: function(response, message, regex)
+	{
+		var match = message.match(regex);
+		var topic = match[6] ? match[6] : (match[8] ? match[8] : '').toLowerCase();
+		http.get('http://puns.niksudan.com/api/puns/random' + (topic ? '?topic=' + topic : ''), function(res) {
+			var pun = '';
+			res.on('data', function(chunk) {
+				pun += chunk;
+			});
+			res.on('end', function() {
+				pun = JSON.parse(pun);
+				if (pun.error) {
+					response('I couldn\'t think of a ' + topic + ' pun');
+				} else {
+					if (topic != '') {
+						if (pun.topics.length == 0) {
+							topic = 'indescribable'
+						} else {
+							topic = pun.topics[0];
+						}
+					}
+					response('Here\'s a ' + topic + ' pun' + '... "' + pun.content + '"');
+				}
+			});
+		});
 	}
 };
